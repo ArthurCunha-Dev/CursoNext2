fa6be43035bc1e299e9becb20eef4870c56925bb
ArthurCunha-Dev <arthurcunha1114@gmail.com>
2024-11-19 16:57:59 -0300

    Página de not found

--- src/app/products-dynamic/[productId]/page.tsx
+++ src/app/products-dynamic/[productId]/page.tsx
@@ -1,4 +1,10 @@
-export default function Info({ params }) {
+interface InfoParams{
+    params:{
+        productId: number
+    }    
+}
+
+export default function Info({ params }: InfoParams) {
     return (
         <div>
             <p>Informação do produto {params.productId}</p>
